5011cce668a384721c06562721759c88f1f0dd02
Jorge <jorge@beon.studio>
2020-07-08 08:37:47 -0500

    fixed delete button

--- src/components/comicCard/index.js
+++ src/components/comicCard/index.js
@@ -7,6 +7,7 @@ import classNames from "classnames";
 
 import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 class ComicCard extends Component {
   constructor(props){
@@ -40,7 +41,7 @@ class ComicCard extends Component {
         {comics.map((row, index) =>(
           <div key = {index} className= {classNames(styles.comic)}>
             <IconButton aria-label="delete" className= {classNames(styles.overlay)} onClick = {() => this.deleteComic(row)} >
-              <DeleteIcon fontSize="large" color="secondary" />
+              <CancelIcon fontSize="large"/>
             </IconButton>
             <Image src={row.thumbnail.path +"/portrait_incredible." + row.thumbnail.extension} rounded />
             <div className= {classNames(styles.name)}> {row.title} </div>
